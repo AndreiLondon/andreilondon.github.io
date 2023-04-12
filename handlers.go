897dd72956454602919d15bd9bd5d92444afb07d
AndrewLondon <andrewmart@mail.com>
2023-04-12 19:47:05 +0100

    Updated

--- handlers.go
+++ handlers.go
@@ -4,16 +4,31 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 var posts = []Post{}
 
 func signupHandler(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/signup.html", "templates/header.html", "templates/footer.html")
-
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	if r.URL.Path != "/signup" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	username := r.FormValue("username")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	if username == "" || email == "" || password == "" {
+		http.Error(w, "400 Bad Request.", http.StatusBadRequest)
+		return
+	}
+
 	t.ExecuteTemplate(w, "signup", nil)
 
 }
@@ -70,4 +85,34 @@ func savePostHanlder(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 
+}
+
+func signUpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userName := strings.TrimSpace(r.FormValue("username"))
+	email := strings.TrimSpace(r.FormValue("email"))
+	password := strings.TrimSpace(r.FormValue("password"))
+
+	if userName == "" || email == "" || password == "" {
+		http.Error(w, "400 Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	// Perform user registration logic, e.g., save to database, generate session ID, etc.
+	// Replace the placeholders with your actual implementation.
+	sessionID, err := registerUser(userName, email, password)
+	if err != nil {
+		http.Error(w, "500 Internal Server Error. Error while registering user", http.StatusInternalServerError)
+		return
+	}
+
+	// Set session ID as a cookie for authenticated session
+	setCookie(w, sessionID)
+
+	// Redirect to homepage after successful registration
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
